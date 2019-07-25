194229ad6a49b275d899397607a036f368f47d9e
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-07-25 21:57:30 +0200

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Mustafa Ozhan on 25/07/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
